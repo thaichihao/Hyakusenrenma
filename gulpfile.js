80265ed56ff0f0a88bfbf41284d22bd179f7b529
thaichihao <thaihao.mail@gmail.com>
2017-11-19 17:12:32 +0700

    finally

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass',function(){
+  return gulp.src('assets/sass/index.sass')
+  .pipe(sass())
+  .pipe(gulp.dest('dist/css/'))
+  .pipe(browserSync.stream());
 });
+
+gulp.task('serve', ['sass'], function() {
+      browserSync.init({
+          server: "./"
+      });
+  
+      gulp.watch("assets/sass/*.sass", ['sass']);
+      gulp.watch("./*.html").on('change', browserSync.reload);
+  });
+
+gulp.task('default',['serve']);
